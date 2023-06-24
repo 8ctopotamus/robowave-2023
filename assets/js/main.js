300dee26176544e2fdbb4479cc59d1e2042e3501
Josh <zylo.codes@gmail.com>
2023-06-24 13:40:23 -0500

    wip: fullpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello robo!')
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    licenseKey: 'gplv3-license',
+    sectionsColor: ['#333738', '#333738', '#333738'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Welcome', 'New', 'Used']
+  })
+})
