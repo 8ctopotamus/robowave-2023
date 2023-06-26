d1dac7722f54dd9d22d58ea63e2260e1a48f9fcb
Josh <zylo.codes@gmail.com>
2023-06-26 16:36:30 -0500

    robot cards, glitch effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
+  $('#intro .btn').on('click', function() {
+    fullpage_api.moveSectionDown()
+  })
+
+  // fullpage scrollable layout
   $('#fullpage').fullpage({
     licenseKey: 'gplv3-license',
     sectionsColor: ['#333738', '#333738', '#333738'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips: ['Welcome', 'New', 'Used']
+  })
+
+  // typewriter effect
+  $('.lead').each(function() {
+    const innerText = this.innerText
+    new Typewriter(this, {
+      loop: true
+    })
+      .typeString(innerText)
+      .pauseFor(1000)
+      .start()
+  })
+
+  // glitchiness
+  PowerGlitch.glitch('.glitch', {
+    // playMode: 'hover',
+    hideOverflow: true,
   })
 })
